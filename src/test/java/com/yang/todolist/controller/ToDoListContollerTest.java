473d4dd8880fcc56852f19dc88159238f35ef917
yhz <1416244864@>
2019-01-23 01:06:18 +0800

    控制器单元测试

--- src/test/java/com/yang/todolist/controller/ToDoListContollerTest.java
+++ src/test/java/com/yang/todolist/controller/ToDoListContollerTest.java
@@ -2,21 +2,30 @@ package com.yang.todolist.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 public class ToDoListContollerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
+
     @Test
-    public void findAll() {
-        System.out.println(123);
+    public void findAll() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/findall"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
     public void insertOrUpdate() {
-        findAll();
-        findAll();
     }
 }
