c8d07a4b9732a0605ba2b2871e80dfa92fc9d47b
yhz <1416244864@>
2019-01-25 21:31:12 +0800

    图片上传配置 3个配置文件yml

---
+++ src/main/java/com/yang/todolist/controller/UploadController.java
@@ -0,0 +1,38 @@
+package com.yang.todolist.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+public class UploadController {
+
+    @Value("${web.upload-path}")
+    private String IMGPATH;
+
+    @PostMapping(value = "/fileUpload")
+    public String fileUpload(@RequestParam(value = "file") MultipartFile file) {
+        if (file.isEmpty()) {
+            System.out.println("文件为空空");
+        }
+        String fileName = file.getOriginalFilename();  // 文件名
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));  // 后缀名
+        fileName = UUID.randomUUID() + suffixName; // 新文件名
+        File dest = new File(IMGPATH + fileName);
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdirs();
+        }
+        try {
+            file.transferTo(dest);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileName;
+    }
+}
